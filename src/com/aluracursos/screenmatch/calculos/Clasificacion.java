8eea7a539e9e6e280c613b34ac5d9526aea527f9
Daniel-tic <daniel.tic2300@gmail.com>
2024-06-17 23:12:22 -0500

    subiendo interfaz

--- src/com/aluracursos/screenmatch/calculos/Clasificacion.java
+++ src/com/aluracursos/screenmatch/calculos/Clasificacion.java
@@ -3,4 +3,4 @@ package com.aluracursos.screenmatch.calculos;
 public interface Clasificacion {
     int getClasificacion();
 }
-//uso de interfaces, como un contrato
+//uso de interfaces, como un contrato establecido
